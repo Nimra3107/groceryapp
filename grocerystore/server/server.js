3ce4e1526d0abaa5eb175375df67cc9c2b2b3e88
Nimra Naveed <153927294+Nimra3107@users.noreply.github.com>
2025-08-25 13:52:05 +0500

    Update server.js

--- grocerystore/server/server.js
+++ grocerystore/server/server.js
@@ -27,7 +27,7 @@ await connectDB();
 await connectCloudinary();
 
 //Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://groceryapp-opal.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
